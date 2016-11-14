2abb93d7922d0998364a29024f98f63a244a1521
BrianSipple <bsipple57@gmail.com>
2016-11-14 00:46:16 -0800

    add polish to grid positioning

--- app/components/project-card.js
+++ app/components/project-card.js
@@ -3,14 +3,9 @@ import injectService from 'ember-service/inject';
 
 export default Component.extend({
   mediaService: injectService('media'),
-  tagName: 'a',
   classNames: [
     'c-project-card',
     'u-relative',
-    'o-flex-grid',
-    'o-flex-grid--justifyCenter',
-    'o-flex-grid--center',
-    'u-pointer',
     'u-overflow-hide'
   ],
   classNameBindings: ['mediaService.classNames'],
