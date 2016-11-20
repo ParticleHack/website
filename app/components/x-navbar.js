a9c905ced225049dd4377d541f25070c65dfbbc1
BrianSipple <bsipple57@gmail.com>
2016-11-20 04:10:52 -0800

    Initial Structure and styling for main project page and transition to details

--- app/components/x-navbar.js
+++ app/components/x-navbar.js
@@ -1,4 +1,14 @@
-import Ember from 'ember';
+import Component from 'ember-component';
+import injectService from 'ember-service/inject';
 
-export default Ember.Component.extend({
+export default Component.extend({
+  tagName: 'header',
+  mediaService: injectService('media'),
+
+  classNames: [
+    'c-navbar',
+    'o-content-box',
+    'u-relative'
+  ],
+  classNameBindings: ['media.classNames']
 });
