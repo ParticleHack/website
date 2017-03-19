4dce2818e41bbc0cb6113c835f04725ce7e8976e
BrianSipple <brsipple42@gmail.com>
2017-03-18 23:12:47 -0700

    add script for deploying files to root github repo

---
+++ bin/deploy-to-gh-pages-root.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+REPO_URL="git@github.com:ParticleHack/particlehack.github.io.git"
+REPO_NAME="particlehack.github.io"
+COMMIT=`git log -1 --oneline`
+
+git clone $REPO_URL
+rsync -a build/* $REPO_NAME
+
+cd $REPO_NAME
+git add --all
+git commit --allow-empty -am "$COMMIT"
+git push origin master
+
